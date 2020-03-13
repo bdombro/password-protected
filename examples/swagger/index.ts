1a80172a881f876e08d4eecd74d9395d19e94c1b
Brian <bdombro@gmail.com>
2020-03-13 19:29:05 -0400

    Added example

--- examples/swagger/index.ts
+++ examples/swagger/index.ts
@@ -1,8 +1,8 @@
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
+import * as shrinkRay from 'shrink-ray-current';
 import passwordProtected from '../../src';
-const shrinkRay = require('shrink-ray-current');
 const repo = require('../../package.json').repository.url.slice(4);
 
 const app = express();
@@ -10,8 +10,6 @@ app.use(cookieParser());
 app.use(bodyParser());
 app.use(shrinkRay());
 app.use(passwordProtected({
-    password: 'superpassword',
-    jwtSecret: 'supersecret',
     hint: `Hint: password is 'superpassword'. Source code for this app is at <a target='_blank' href='${repo}'>${repo}</a>`,
 }));
 app.use(express.static(__dirname + "/private")); // include any other files dist directory
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,9 @@
 export interface passwordProtectedProps {
+    pageTitle?: string;
     password?: string;
     jwtSecret?: string;
-    pageTitle: string;
     hint?: string;
     jwtData?: any;
     loginHtml?: string;
 }
-export default function passwordProtected({ password, jwtSecret, pageTitle, hint, jwtData, loginHtml }: passwordProtectedProps): (req: any, res: any, next: any) => void;
+export default function passwordProtected({ pageTitle, password, jwtSecret, hint, jwtData, loginHtml }: passwordProtectedProps): (req: any, res: any, next: any) => void;
