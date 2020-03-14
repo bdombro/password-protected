8605b46c3d7e373f21e873a88c01899db2b68555
Brian <bdombro@gmail.com>
2020-03-13 22:32:25 -0400

    v1.0.16

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface passwordProtectedProps {
-    pageTitle?: string;
-    password?: string;
-    jwtSecret?: string;
-    hint?: string;
-    jwtData?: any;
-    loginHtml?: string;
-}
-export default function passwordProtected({ pageTitle, password, jwtSecret, hint, jwtData, loginHtml }: passwordProtectedProps): (req: any, res: any, next: any) => void;
